f23d390239256edcef402f76fb98b9340a00102e
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 08:49:22 -0500

    feat: :ambulance: Math.random()
    se agrego y explico el metodo Math.random() con algunas especificaciones

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-/* Math.pow
+/* Math.random()
 
-El método Math.pow() devuelve el valor de x elevado a y (xy).
-Basicamente eleva el primer valor al numero que se pone luego
+
+El método Math.random() devuelve un número aleatorio desde 0 (inclusive) hasta 1 (exclusivo), pero sin incluirlo.
+Basicamente pone un limite en 0.99 haciendo que el 1 no sea opcion posible para sacar.
+
 */
-let x = Math.pow(4, 3); // muestra 64, es como poner 4 elevado a la 3, 4*4 = 16; 16*4 = 64
-let a = Math.pow(0, 1); // muestra 0 
-let b = Math.pow(1, 1); // muestra 1 
-let c = Math.pow(1, 10); // muestra 1
-let d = Math.pow(3, 3); // muestra 27 
-let e = Math.pow(-3, 3); // muestra -27
-let f = Math.pow(2, 4); // muestra 16
+let x = Math.random() * 10;  //numero random entre 0 y 10
+
+let y = Math.floor((Math.random() * 100) + 1); //cualquier numero ENTERO entre 1 y 100
