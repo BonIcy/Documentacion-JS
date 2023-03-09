4b5ae140b332b03262b476a8097334f6917e492b
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 09:14:13 -0500

    feat: :ambulance: Math.trunc() & toFixed()
    se explicaron y ejemplificaron los metodos Math.trunc() & toFixed()

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-/* Math.floor()
+/* Math.trunc() & toFixed()
 
-El método Math.floor() redondea un número HACIA ABAJO al entero más cercano.
-Lo mismo que en ceil, solo que aca redondea hacia ABAJO
+El método Math.trunc() devuelve la parte entera de un número.
+El método Math.trunc() elimina los decimales (NO redondea el número).
+
+El método toFixed() convierte un número en una cadena.
+El método toFixed() redondea la cadena a un número específico de decimales.
+
 
 */
-let a = Math.ceil(0.60);//0
-let b = Math.ceil(0.40);// 0
-let c = Math.ceil(5);// 5
-let d = Math.ceil(5.1);// 5 
-let e = Math.ceil(-5.1);//-6
-let f = Math.ceil(-5.9);//-6
+let u = Math.trunc(8.76); // devuelve la parte ENTERA del numero, es decir, 8, sin tener en cuenta los decimales
+
+let num = 5.56789;
+let n = num.toFixed(); //redondea a 6 ya que si aplica los decimales y el aproximamiento
+
+let nume = 5.56789;
+let nn = nume.toFixed(2); // redondea a 5.57 ya que le indicamos que queremos ver 2 decimales
