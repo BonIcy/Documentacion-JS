25759e0ae6cedfc96d31c71553e8e38ffb961046
Pablo Ramirez <pablocampus2023@gmail.com>
2023-03-09 08:33:00 -0500

    feat: :ambulance: Math.max() & Math.min()
    Se explico y ejemplifico el Math.max() y el Math.min()

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-/* Math.PI
+/* Math.max() & Math.min()
 
-Math.PI es una característica de ECMAScript1 (ES1).
+El método Math.max() devuelve el número con el valor más alto.
 
-ES1 (JavaScript 1997) es totalmente compatible con todos los navegadores:
-
+El método Math.min() devuelve el número con el valor más bajo.
 */
+let x = Math.max(5, 10); // regresa el 10
 
-let x = Math.PI; // define a x como numero PI 3.141592653589793
+let y = Math.min(5, 10); // regresa el 5
+
+let a = Math.max(20, -50); //regresa el 20
+let b = Math.max(0, 150, 30, 20, 38); //regresa el 150
+let c = Math.max(-5, 10); //regresa el 10
+let d = Math.max(-5, -10); //regresa el -5
+let e = Math.max(1.5, 2.5); //regresa 2.5
+
+let aa = Math.min(20, -50); //regresa el -50
+let bb = Math.min(0, 150, 30, 20, 38);//regresa el 0
+let cc = Math.min(-5, 10); //regresa el -5
+let dd = Math.min(-5, -10);//regresa el -10
+let ee = Math.min(1.5, 2.5); //regresa el 2.5
