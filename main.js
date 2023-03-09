4825e605b70ecc887a8a1e930c58dc53a645f015
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 08:42:32 -0500

    feat: :ambulance: Math.pow
    se explico y aplico el metodo Math.pow con ejemplos

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
-/* Math.max() & Math.min()
+/* Math.pow
 
-El método Math.max() devuelve el número con el valor más alto.
-
-El método Math.min() devuelve el número con el valor más bajo.
+El método Math.pow() devuelve el valor de x elevado a y (xy).
+Basicamente eleva el primer valor al numero que se pone luego
 */
-let x = Math.max(5, 10); // regresa el 10
-
-let y = Math.min(5, 10); // regresa el 5
-
-let a = Math.max(20, -50); //regresa el 20
-let b = Math.max(0, 150, 30, 20, 38); //regresa el 150
-let c = Math.max(-5, 10); //regresa el 10
-let d = Math.max(-5, -10); //regresa el -5
-let e = Math.max(1.5, 2.5); //regresa 2.5
-
-let aa = Math.min(20, -50); //regresa el -50
-let bb = Math.min(0, 150, 30, 20, 38);//regresa el 0
-let cc = Math.min(-5, 10); //regresa el -5
-let dd = Math.min(-5, -10);//regresa el -10
-let ee = Math.min(1.5, 2.5); //regresa el 2.5
+let x = Math.pow(4, 3); // muestra 64, es como poner 4 elevado a la 3, 4*4 = 16; 16*4 = 64
+let a = Math.pow(0, 1); // muestra 0 
+let b = Math.pow(1, 1); // muestra 1 
+let c = Math.pow(1, 10); // muestra 1
+let d = Math.pow(3, 3); // muestra 27 
+let e = Math.pow(-3, 3); // muestra -27
+let f = Math.pow(2, 4); // muestra 16
