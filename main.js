c0fe843daf0bda2518fdb17d5959b909eada1474
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 08:51:46 -0500

    feat: :ambulance: Math.sqrt()
    se explico y ejemplifico el metodo Math.sqrt()

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-/* Math.random()
+/* Math.sqrt()
 
-
-El método Math.random() devuelve un número aleatorio desde 0 (inclusive) hasta 1 (exclusivo), pero sin incluirlo.
-Basicamente pone un limite en 0.99 haciendo que el 1 no sea opcion posible para sacar.
+El método Math.sqrt() devuelve la raíz cuadrada de un número.
 
 */
-let x = Math.random() * 10;  //numero random entre 0 y 10
-
-let y = Math.floor((Math.random() * 100) + 1); //cualquier numero ENTERO entre 1 y 100
+let x = Math.sqrt(9);// aqui saca el numero 3 ya que es la raiz de 9
+let a = Math.sqrt(0); //0
+let b = Math.sqrt(1); //1
+let c = Math.sqrt(9); //3
+let d = Math.sqrt(64); //8
+let e = Math.sqrt(-9); //NaN
