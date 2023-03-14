7ac7a2adbfafd0ecaddfdc8e974f54a8137aba79
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-14 17:06:35 -0500

    feat: :ambulance: clausuras/cierres
    se explican que son las clausuras y como funcionan

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 /* 
 FUNCIONES:
-Funciones autoejecutables
+Clausuras
 
-Pueden existir casos en los que necesites crear una función y ejecutarla sobre la marcha. En Javascript es muy sencillo crear funciones autoejecutables. Básicamente, sólo tenemos que envolver entre paréntesis la función anónima en cuestión (no necesitamos que tenga nombre, puesto que no la vamos a guardar) y luego, ejecutarla:
-*/
+Las clausuras o cierres, es un concepto relacionado con las funciones y los ámbitos que suele costar comprender cuando se empieza en Javascript. Es importante tener las bases de funciones claras hasta este punto, lo que permitirá entender las bases de una clausura.
 
-// Función autoejecutable
-(function () {
-  console.log("Hola!!");
+A grandes rasgos, en Javascript, una clausura o cierre se define como una función que «encierra» variables en su propio ámbito (y que continúan existiendo aún habiendo terminado la función). Por ejemplo, veamos el siguiente ejemplo:*/
+
+// Clausura: Función incr()
+const incr = (function () {
+  let num = 0;
+  return function () {
+    num++;
+    return num;
+  };
 })();
 
-// Función autoejecutable con parámetros
-(function (name) {
-  console.log(`¡Hola, ${name}!`);
-})("Manz");
+typeof incr; // 'function'
+incr(); // 1
+incr(); // 2
+incr(); // 3
 /*
-De hecho, también podemos utilizar parámetros en dichas funciones autoejecutables. Observa que sólo hay que pasar dichos parámetros al final de la función autoejecutable.
+Tenemos una función anónima que es también una función autoejecutable. Aunque parece una función por expresión, no lo es, ya que la variable incr está guardando lo que devuelve la función anónima autoejecutable, que a su vez, es otra función diferente.
 
-Ten en cuenta, que si la función autoejecutable devuelve algún valor con return, a diferencia de las funciones por expresión, en este caso lo que se almacena en la variable es el valor que devuelve la función autoejecutada:*/
+La «magia» de las clausuras es que en el interior de la función autoejecutable estamos creando una variable num que se guardará en el ámbito de dicha función, por lo tanto existirá con el valor declarado: 0.
 
-const f = (function (name) {
-  return `¡Hola, ${name}!`;
-})("Manz");
-
-f; // '¡Hola, Manz!`
-typeof f; // 'string'
+Por lo tanto, en la variable incr tenemos una función por expresión que además conoce el valor de una variable num, que sólo existe dentro de incr. Si nos fijamos en la función que devolvemos, lo que hace es incrementar el valor de num y devolverlo. Como la variable incr es una clausura y mantiene la variable en su propio ámbito, veremos que a medida que ejecutamos incr(), los valores de num (que estamos devolviendo) conservan su valor y se van incrementando.*/
