2906c2222965d3914fc9dfa7b68202b69a1601c7
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-14 08:55:37 -0500

    feat: :ambulance: funciones como objetos
    se  explican las funciones como objetos con un corto ejemplo

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 /* 
 FUNCIONES
 
-Funciones por expresión
+Funciones como objetos
 
-Sin embargo, en Javascript es muy habitual encontrarse códigos donde los programadores «guardan funciones» dentro de variables, para posteriormente «ejecutar dichas variables». Se trata de un enfoque diferente, creación de funciones por expresión, que fundamentalmente, hacen lo mismo con algunas diferencias:
-*/
+Como curiosidad, debes saber que se pueden declarar funciones como si fueran objetos. Sin embargo, es un enfoque que no se suele utilizar en producción. Simplemente es interesante saberlo para darse cuenta que en Javascript todo pueden ser objetos:*/
 
-// El segundo "saludar" (nombre de la función) se suele omitir: es redundante
-const saludo = function saludar() {
-  return "Hola";
-};
-
-saludo(); // 'Hola'
-
-/* Con este nuevo enfoque, estamos creando una función en el interior de una variable, lo que nos permitirá posteriormente ejecutar la variable (como si fuera una función). Observa que el nombre de la función (en este ejemplo: saludar) pasa a ser inútil, ya que si intentamos ejecutar saludar() nos dirá que no existe y si intentamos ejecutar saludo() funciona correctamente. */
+const saludar = new Function("return 'Hola';");
+saludar(); // 'Hola'
